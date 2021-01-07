0e28071adc2ef5390bb78d3db2d866d28461f497
AryanBhatt <aryanbhatt1002@gmail.com>
2021-01-07 15:08:33 +0530

    first commit

---
+++ assets/js/mapbox.init.js
@@ -0,0 +1,28 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ5YW5iaGF0dDEwMDIiLCJhIjoiY2tqaDFmcnloNDFpYTJybnFvbmk0cTliNyJ9.ImW_FZl5b1VBm1bYaLyziA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+	center: [77.11853, 28.53907], // starting position
+    zoom: 13 // starting zoom
+});
+		
+// create the popup
+var popup = new mapboxgl.Popup({ offset: 40 }).setText(
+    'New Delhi, Delhi 110037'
+);
+
+// create DOM element for the marker
+var el = document.createElement('div');
+el.id = 'marker';
+ 
+// create the marker
+new mapboxgl.Marker(el)
+    .setLngLat([77.11853, 28.53907])
+    .setPopup(popup) // sets a popup on this marker
+    .addTo(map);
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+// disable map zoom when using scroll
+map.scrollZoom.disable();
