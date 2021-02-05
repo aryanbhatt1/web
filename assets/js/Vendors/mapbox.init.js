0c20f1144034b5c2ab457b65a0d12d1269c2000a
AryanBhatt <aryanbhatt1002@gmail.com>
2021-02-05 10:35:53 +0530

    commit

--- assets/js/Vendors/mapbox.init.js
+++ assets/js/Vendors/mapbox.init.js
@@ -5,7 +5,7 @@ var map = new mapboxgl.Map({
 	center: [77.11853, 28.53907], // starting position
     zoom: 13 // starting zoom
 });
-		
+
 // create the popup
 var popup = new mapboxgl.Popup({ offset: 40 }).setText(
     'New Delhi, Delhi 110037'
@@ -14,12 +14,22 @@ var popup = new mapboxgl.Popup({ offset: 40 }).setText(
 // create DOM element for the marker
 var el = document.createElement('div');
 el.id = 'marker';
- 
+
 // create the marker
 new mapboxgl.Marker(el)
     .setLngLat([77.11853, 28.53907])
     .setPopup(popup) // sets a popup on this marker
     .addTo(map);
+
+// Add geolocate control to the map.
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    })
+);
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
